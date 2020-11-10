0fba4f9e4fac36fe0957820a03af9f30c61a6411
bappi2097 <bappi35-2097@diu.edu.bd>
2020-11-10 12:42:46 +0600

    single student id print Bug fixed

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -10,7 +10,6 @@ class PrintController extends Controller
 {
     public function printId(Student $student)
     {
-        $student = Student::first();
         $pdf = PDF::loadView('id-card-pdf', ["students" => [$student]]);
         $pdf->setPaper('A4', '');
         return $pdf->stream($student->first_name . "_" . $student->last_name . "-" . str_pad($student->id + 1, 4, '0', STR_PAD_LEFT) . '.pdf');
